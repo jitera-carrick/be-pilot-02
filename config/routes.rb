90dfb2531bd5d87ee290c310c472d550b5086774
jitera-carrick <hongnhat.nguyen@jitera.com>
2024-09-09 11:29:09 +0700

    main

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  resources :products
   resources :stores
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
---
+++ spec/factories/products.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :product do
+    name { "MyString" }
+    description { "MyString" }
+    price { "9.99" }
+  end
+end
